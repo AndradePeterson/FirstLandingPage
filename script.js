8d857dab1b767ffb4f67dd8a5c96ddafed618d21
AndradePeterson <45950601+AndradePeterson@users.noreply.github.com>
2023-04-19 20:03:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,12 @@ window.onscroll = function(){scrollFunction()}
 
 function scrollFunction(){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        document.querySelector(".meuHeader").style.height = "80px";
-        document.querySelector(".logo").style.height = "80px";
-        document.querySelector("#nameLogo").style.font = "40px";
+        document.querySelector(".meuHeader").style.height = "5em";
+        document.querySelector(".logo").style.height = "5em";
+        document.querySelector("#nameLogo").style.height = "1.3em";
     } else{
-        document.querySelector(".meuHeader").style.height = "110px";
-        document.querySelector(".logo").style.height = "110px";
-        document.querySelector("#nameLogo").style.height = "40px";
+        document.querySelector(".meuHeader").style.height = "7em";
+        document.querySelector(".logo").style.height = "7em";
+        document.querySelector("#nameLogo").style.height = "1.3em";
     }
 }
-
-var tituloEnaltecerBrasil = document.getElementById("tituloEnaltecerBrasil");
-
-var opacity = 1; //define a opacidade inicial do elemento
-
-var intervalId = setInterval(function() {
-    opacity -= 0.1; //decrementa a opacidade do elemento
-
-    if(opacity <= 0.8){
-        opacity = 1; //redefine a opacidade para 1 e inverte a direção da pulsação
-    }
-
-    tituloEnaltecerBrasil.style.opacity = opacity;
-}, 300);
